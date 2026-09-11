638c096fb4c4bd9e7cd1d93a6c773072839b417e
Harshit gangwar <harshitgangwar444@gmail.com>
2026-09-11 23:00:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showMessage() {
-
-    const message = document.getElementById("hiddenMessage");
-
-    message.innerHTML =
-        "Bas ek simple si baat — jab bhi possible ho, message kar dena. 😊";
-
-}
